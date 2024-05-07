5acda317f3d91ffef062d7d60ec791ee7e4659c9
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-07 17:04:12 +0900

    Title: 변수 선언하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240507/변수 선언하기/declaring-variables.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a = 3;
+        String b = "C";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
