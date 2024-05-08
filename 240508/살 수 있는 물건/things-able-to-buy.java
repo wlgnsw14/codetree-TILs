45d9315b4f30c19fe64809d4b6c2f4b46d43a83e
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-08 17:12:44 +0900

    Title: 살 수 있는 물건, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a >= 3000){
+            System.out.print("book");   
+        }else if(a >= 1000){
+            System.out.print("mask");
+        }else{
+            System.out.print("no");
+        }
+    }
+}
