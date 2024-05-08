795e2f158fefd20042aef7b85300df2e59df60c9
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-08 18:44:22 +0900

    Title: 장학금, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240508/장학금/scholarship.java
+++ 240508/장학금/scholarship.java
@@ -6,10 +6,10 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        if(a >= 90 && b >= 90){
-            System.out.print(50000);
-        }else if(a >= 90 && b >= 95){
+        if(a >= 90 && b >= 95){
             System.out.print(100000);
+        }else if(b >= 90){
+            System.out.print(50000);
         }else{
             System.out.print(0);
         }
