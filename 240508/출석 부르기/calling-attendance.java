081b45a4ca2d4a2088cb64b5753d2ccb93eb2909
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-08 17:14:13 +0900

    Title: 출석 부르기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/출석 부르기/calling-attendance.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a == 1){
+            System.out.print("John");   
+        }else if(a == 2){
+            System.out.print("Tom");
+        }else if(a == 3){
+            System.out.print("Paul");
+        }
+    }
+}
