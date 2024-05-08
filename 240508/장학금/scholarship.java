d5bc2aa288e26a1852c6780b7673304217d97b2f
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-08 18:43:01 +0900

    Title: 장학금, Time: 158ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/장학금/scholarship.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a >= 90 && b >= 90){
+            System.out.print(50000);
+        }else if(a >= 90 && b >= 100){
+            System.out.print(100000);
+        }else{
+            System.out.print(0);
+        }
+    }
+}
