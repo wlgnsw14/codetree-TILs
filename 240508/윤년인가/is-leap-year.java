990c776fb919b586162f07647aeff00fcf1a6967
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-08 19:28:58 +0900

    Title: 윤년인가, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/윤년인가/is-leap-year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if(a%4==0){
+            if(a%100 == 0 && a%400 != 0){
+                System.out.print("false");
+            }else{
+                System.out.print("true");
+            }
+        }else{
+            System.out.print("false");
+        }
+    }
+}
