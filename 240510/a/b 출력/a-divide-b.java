e8fb3db9dfa1f24bfb0dd3e970357954001e5cbd
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 17:16:13 +0900

    Title: a/b 출력, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240510/a/b 출력/a-divide-b.java
+++ 240510/a/b 출력/a-divide-b.java
@@ -7,12 +7,16 @@ public class Main {
         int b = sc.nextInt();
         int c = 0;
         int d = 0;
+        int e = a%b;
 
         System.out.print((a/b)+".");
 
         for(int i = 0; i <= 19; i++){
             if(a/b >= 1){
-                
+                c = (e*10)/b;
+                d = (e*10)%b;
+                System.out.print(c);
+                a = d;
             }else{
                 c = (a*10)/b;
                 d = (a*10)%b;
