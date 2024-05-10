cbef96b824824558e75d30c40439ecc73810567e
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 16:31:59 +0900

    Title: 감소시키며 출력하기 2, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240510/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 26;
+        while(a >= 10){
+            System.out.print(a+" ");
+            a--;
+        }
+    }
+}
