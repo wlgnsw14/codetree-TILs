122c286f2d6503416f94aa32d93e5eff2ee5e2a0
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 16:38:11 +0900

    Title: 문자 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240510/문자 출력하기/print-text.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for(int i = 0; i < 8; i++){
+            System.out.print(a);
+        }
+    }
+}
