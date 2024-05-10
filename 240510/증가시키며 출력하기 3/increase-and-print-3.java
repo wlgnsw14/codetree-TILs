e1adec7470835023058caa967a813c81143f33d2
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 16:11:35 +0900

    Title: 증가시키며 출력하기 3, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240510/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        while(a <= 26){
+            System.out.print(a+" ");
+            a += 2;
+        }
+    }
+}
