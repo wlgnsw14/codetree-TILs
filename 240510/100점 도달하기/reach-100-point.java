681420b2c23b0f7736ba8d24554e1854bf30a14d
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 18:05:22 +0900

    Title: 100점 도달하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240510/100점 도달하기/reach-100-point.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for(int i = a; i <= 100; i++){
+            if(a >= 90){
+                System.out.print("A ");
+            }else if(a >= 80){
+                System.out.print("B ");
+            }else if(a >= 70){
+                System.out.print("C ");
+            }else if(a >= 60){
+                System.out.print("D ");
+            }else{
+                System.out.print("F ");
+            }
+            a++;
+        }
+    }
+}
