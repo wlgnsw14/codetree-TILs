cf023f704d4e1ca7cca5a1ab64a97931cdb0395b
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 16:59:18 +0900

    Title: a/b 출력, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240510/a/b 출력/a-divide-b.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = 0;
+        int d = 0;
+
+        System.out.print("0.");
+
+        for(int i = 0; i <= 19; i++){
+            c = (a*10)/b;
+            d = (a*10)%b;
+            System.out.print(c);
+            a = d;
+        }
+    }
+}
