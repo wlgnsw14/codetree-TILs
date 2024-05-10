7ddc6a33b792f74dea64d81b13ed686721723ca4
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-10 18:15:14 +0900

    Title: a 부터 b 까지, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240510/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = a; i <= b;){
+            System.out.print(i+" ");
+            if(i%2==1){
+                i *= 2;
+            }else{
+                i += 3;
+            }
+        }
+    }
+}
