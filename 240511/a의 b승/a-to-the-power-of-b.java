2c520dc0145abb6cdfd41ea02149a3db5871ce00
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-11 22:15:17 +0900

    Title: a의 b승, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240511/a의 b승/a-to-the-power-of-b.java
+++ 240511/a의 b승/a-to-the-power-of-b.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int prod = a;
+        int prod = 1;
 
-        for(int i = 2; i <= b; i++){
+        for(int i = 1; i <= b; i++){
             prod *= a;
         }
         System.out.print(prod);
