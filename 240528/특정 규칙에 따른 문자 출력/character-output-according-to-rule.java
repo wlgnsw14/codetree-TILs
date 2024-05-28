eda7abb315049a857ab05d70a213cd646d07a942
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-05-28 16:42:56 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) { 
+            for (int j = 0; j < a - 1 - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("@ ");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < a - 1; i++) {
+            for (int j = 0; j < a - 1 - i; j++) {
+                System.out.print("@ ");
+            }
+            System.out.println("");
+        }
+    }
+}
