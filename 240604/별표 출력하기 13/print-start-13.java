0ea8e986284b713f17c2212d518d111268c93c01
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-06-04 23:12:42 +0900

    Title: 별표 출력하기 13, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for(int i = 0; i < a * 2; i++){
+            if(i%2==0){
+                for(int j = 0; j < a - i / 2; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }else{
+                for(int j = 0; j < (i - 1) / 2 + 1; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
