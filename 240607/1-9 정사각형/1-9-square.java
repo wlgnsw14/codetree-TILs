7d81c247f11dd3fdc7f67a31e3fed1769d652db9
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-06-07 18:47:54 +0900

    Title: 1-9 정사각형, Time: 236ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/1-9 정사각형/1-9-square.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int cnt = 1;
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print(cnt);
+                if(cnt == 9){
+                    cnt = 1;
+                }else{
+                    cnt++;
+                } 
+            }
+            System.out.println();
+        }
+    }
+}
