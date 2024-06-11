bfc98fff07b1ab3db4b66b239eb1fb6c665a3dde
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-06-11 23:39:39 +0900

    Title: 정상적인 구구단, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240611/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a; j++) {
+                System.out.print(i + " * " + j + " = " + (i * j));
+                if (j < a) 
+                    System.out.print(", ");
+            }
+            System.out.println();
+        }
+    }
+}
