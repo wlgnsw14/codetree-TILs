4e69b9a2478e1069f441b9e70ae03213a8db789a
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-06-22 23:48:51 +0900

    Title: 알파벳 출력하기 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240622/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int cnt = 65;
+
+
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < a; j++){
+                if(j < i){
+                    System.out.print("  ");
+                }else{
+                    if(cnt == 91){
+                        cnt = 65;
+                        System.out.print((char)(cnt) + " ");
+                        cnt++;
+                    }else{
+                        System.out.print((char)(cnt) + " ");
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
