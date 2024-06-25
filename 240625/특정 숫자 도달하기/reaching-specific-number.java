3f1b4f7000e24ecb1aa449451694ccd2759547d3
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-06-25 19:10:34 +0900

    Title: 특정 숫자 도달하기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240625/특정 숫자 도달하기/reaching-specific-number.java
+++ 240625/특정 숫자 도달하기/reaching-specific-number.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int[] arr = new int[10];
         int sum = 0;
         int cnt = 0;
-        double avg = 0.0;
 
         for (int i = 0; i < 9; i++) {
             arr[i] = sc.nextInt();
-        }
-
-        while(true){
             if(arr[cnt] >= 250){
                 break;
             }else{
@@ -21,7 +16,8 @@ public class Main {
                 cnt++;
             }
         }
-        avg = (double) sum/cnt;
+
+        double avg = (double) sum/cnt;
         System.out.print(sum+" ");
         System.out.printf("%.1f", avg);
     }
