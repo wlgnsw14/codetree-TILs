9302a5b54a70b64ab45dce4f3e4f72ccca43ab98
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-07-15 00:00:19 +0900

    Title: 100 도달하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240715/100 도달하기/reach-100.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        arr[0] = 1;
+        arr[1] = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 2; i < 10; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+            cnt++;
+            if(arr[i] >= 100){
+                break;
+            }   
+        }
+
+        for (int i = 0; i < cnt + 2; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
