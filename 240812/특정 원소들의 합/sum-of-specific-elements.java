d799cf9750dadf62be16c41976f1ae78e86e8f99
wlgnsw14 <140688911+wlgnsw14@users.noreply.github.com>
2024-08-12 04:17:43 +0900

    Title: 특정 원소들의 합, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240812/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr2d = new int[4][4];
+        int n = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr2d[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 1+i; j++) {
+                n += arr2d[i][j];
+            }
+        }
+        System.out.println(n);
+    }
+}
